9f150af504e951b888eb694bf24802794c81c61a
Jonathan Matta Romualdo <33607760+jmattaro@users.noreply.github.com>
2025-01-26 15:29:42 -0500

    Update D-solid-solution.py
    Solid - parte 1

--- solid/D-solid-solution.py
+++ solid/D-solid-solution.py
@@ -1,1 +1,33 @@
-# Here your solution
+from abc import ABC, abstractmethod
+class PaymentType(ABC):
+    @abstractmethod
+    def payment(self,acount:str,amount:float):
+        pass
+
+class PaymentPaypal(PaymentType):
+    def payment(self, acount:str, amount:float):
+        print("Pago realizado por medio de Paypal")
+
+class PaymentCredirCard(PaymentType):
+    def payment(self, acount:str, amount:float):
+        print("Pago realizado por medio de Tarejeta de Credito")
+
+
+class PaymentBitcoins(PaymentType):
+    def payment(self, acount:str, amount:float):
+        print("Pago realizado por medio de bitcoins")
+        
+class PaymentProcessor:
+    def __init__(self, UseMethod: PaymentType):
+        self.MethodPay = UseMethod
+
+    def process_payment(self, acount : str, amount: float):
+        self.MethodPay.payment(acount, amount)
+
+def main():
+    service = PaymentBitcoins()
+    procesar = PaymentProcessor(service)
+    procesar.process_payment("55555555555",300)
+    
+if __name__ == "__main__":
+    main()
